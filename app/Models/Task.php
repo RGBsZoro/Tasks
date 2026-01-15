6ae09cda1cc6dedf486d8a77efdffea24a086524
wasem <wasemalhariri13@gmail.com>
2026-01-15 12:32:24 +0300

    Task_16

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Task extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['title' , 'projecy_id'];
+
+    public function users(){
+        return $this->hasMany(User::class);
+    }
 
     public function images(){
         return $this->morphMany(Image::class , 'imagable');
